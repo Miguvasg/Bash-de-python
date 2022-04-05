3672a36c72bfe8aebbb4ba72e7d051e7035e6f01
Miguel Vásquez <miguvasg@outlook.com>
2022-04-04 20:00:22 -0500

    actualizado el script con buenas prácticas

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 import sys
 
-if len(sys.argv) != 2:
-    print('Error: solo puedes colocar un argumento')
-else:
-    try:
-        print(f'hola, {sys.argv[1]}')
-    except IndexError:
-        print('Error: debes colocar tu nombre como un argumento')
+def main():
+    if len(sys.argv) != 2:
+        print('Error: solo puedes colocar un argumento')
+    else:
+        try:
+            print(f'hola, {sys.argv[1]}')
+        except IndexError:
+            print('Error: debes colocar tu nombre como un argumento')
+
+if __name__ == "__main__":
+    main()
