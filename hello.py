9aba0ad56f749f7a2c02028e2969b16c61aa230d
Miguel Vásquez <miguvasg@outlook.com>
2022-04-04 19:31:49 -0500

    se actualiza el fichero para recibir el nombre como argumento

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
-print('hola desde script, estoy probando correr un script desde bash')
+import sys
+
+if len(sys.argv) != 2:
+    print('Error: solo puedes colocar un argumento')
+else:
+    try:
+        print(f'hola, {sys.argv[1]}')
+    except IndexError:
+        print('Error: debes colocar tu nombre como un argumento')
